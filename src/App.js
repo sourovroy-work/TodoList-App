256d10c4118aface2c8a8c96918b19c1ede4ff00
Sourov Roy <roy.sourov98@gmail.com>
2021-11-26 08:04:14 +0530

    Created TODOList App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './MyComponents/Header'
+import { Todos } from './MyComponents/Todos'
+import { Footer } from './MyComponents/Footer'
+import React, {useState} from 'react'
+import {AddTodo} from './MyComponents/AddTodo'
 
 function App() {
+
+  let onDelete = (todo) => {
+    setTodos(todos.filter((e) => {
+      return e!=todo;
+    }))
+  }
+
+  let addTodo = (title, desc) => {
+    let sno = todos.length? todos[todos.length-1].sno + 1: 1;
+    const newTodo = {
+      sno: sno,
+      title: title,
+      desc: desc
+    }
+    setTodos([...todos, newTodo]);
+  }
+
+ const [todos, setTodos] = useState([
+    {
+      sno: 1,
+      title: "Read On The Height Of Despair",
+      desc: "Aphorism by Emil Cioral"
+    },
+    {
+      sno: 2,
+      title: "Read Beyond Good And Evil",
+      desc: "Book by F. Nietzshe"
+    },
+    {
+      sno: 3,
+      title: "Read Myth Of Sisyphus",
+      desc: "Essay by Albert Camus"
+    }
+  ])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="My Todos List" searchBar={true} />
+      <AddTodo addTodo={addTodo}/>
+      <Todos todos={todos} onDelete={onDelete} />
+      <Footer />
+    </>
   );
 }
 
